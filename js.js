588b1acda19b3732441d611c267c1ee678dd0d01
papunatsiskadze <papunaciskadze@gmail.com>
2022-05-17 22:49:47 +0400

    comit

---
+++ js.js
@@ -0,0 +1,11 @@
+var age = 25;
+var income = 500;
+if (age < 30 || income > 500){
+    alert("შეიშვება ოჯახში")
+}
+else if (age > 30 && income <500){
+    alert("არ შეიშვება ოჯახში")
+}
+else {
+    alert("ეჰჰ")
+}
