2148c1d47907d4897899729dbbc36fb45947c993
papunatsiskadze <papunaciskadze@gmail.com>
2022-05-31 18:04:38 +0400

    cc

--- js.js
+++ js.js
@@ -1,13 +1,18 @@
-cinema = {
-    name:"შმავეა",
-    languages: ["ქართული" , "ინგლისური" , "რუსული"],
+var cinema = {
+    theatre:"კინოთეატრი: შმავეა",
+    languages: ["ენები: ქართული" , "ინგლისური" , "რუსული"],
     premiere:{
-    name:"კუკარაჩა 3D",
-    imdb:9.8
+    name:"ფილმი: კუკარაჩა 3D",
+    imdb:"შეფასება 9.8"
     },
-    cities :[
-    {name:"დოქტორი სტრეინჯი", imdb:8.5},
+    films :[
+    {name:"დოქტორი სტრეინჯი", imdb:"შეფასება 8.5"},
     {name:"ფორსაჟი 23", imdb:1.05},
     {name:"ბეტმენი", imdb:7.7}
     ]
-    }
+};
+
+document.getElementById("description1").innerHTML =(cinema.theatre + "</br>" + cinema.languages + "</br>" + cinema.premiere.name + "</br>" +cinema.premiere.imdb);
+document.getElementById("description2").innerHTML =(cinema.theatre + "</br>" + cinema.languages + "</br>" + cinema.films[0].name + "</br>" + cinema.films[0].imdb);
+document.getElementById("description3").innerHTML =(cinema.theatre + "</br>" + cinema.languages + "</br>" + cinema.films[1].name + "</br>" +cinema.films[1].imdb);
+document.getElementById("description4").innerHTML =(cinema.theatre + "</br>" + cinema.languages + "</br>" + cinema.films[2].name + "</br>" + cinema.films[2].imdb);
